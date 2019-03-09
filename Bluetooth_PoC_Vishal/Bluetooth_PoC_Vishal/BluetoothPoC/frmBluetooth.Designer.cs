16d706ccad0ab18469a528e9e9ccd591771f7096
cutyfeas <vishal.kumar061@gmail.com>
2019-03-09 15:14:38 +0530

    f

---
+++ Bluetooth_PoC_Vishal/Bluetooth_PoC_Vishal/BluetoothPoC/frmBluetooth.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace BluetoothPoC
+{
+    partial class frmBluetooth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDevices = new System.Windows.Forms.ListBox();
+            this.btnDiscover = new System.Windows.Forms.Button();
+            this.btnGetMAC = new System.Windows.Forms.Button();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.Pair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstDevices
+            // 
+            this.lstDevices.FormattingEnabled = true;
+            this.lstDevices.ItemHeight = 16;
+            this.lstDevices.Location = new System.Drawing.Point(17, 16);
+            this.lstDevices.Margin = new System.Windows.Forms.Padding(4);
+            this.lstDevices.Name = "lstDevices";
+            this.lstDevices.Size = new System.Drawing.Size(306, 356);
+            this.lstDevices.TabIndex = 0;
+            // 
+            // btnDiscover
+            // 
+            this.btnDiscover.Location = new System.Drawing.Point(479, 16);
+            this.btnDiscover.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDiscover.Name = "btnDiscover";
+            this.btnDiscover.Size = new System.Drawing.Size(160, 28);
+            this.btnDiscover.TabIndex = 1;
+            this.btnDiscover.Text = "&Device Discovery";
+            this.btnDiscover.UseVisualStyleBackColor = true;
+            this.btnDiscover.Click += new System.EventHandler(this.btnDiscover_Click);
+            // 
+            // btnGetMAC
+            // 
+            this.btnGetMAC.Location = new System.Drawing.Point(479, 52);
+            this.btnGetMAC.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGetMAC.Name = "btnGetMAC";
+            this.btnGetMAC.Size = new System.Drawing.Size(160, 28);
+            this.btnGetMAC.TabIndex = 2;
+            this.btnGetMAC.Text = "Get &MAC Address";
+            this.btnGetMAC.UseVisualStyleBackColor = true;
+            this.btnGetMAC.Click += new System.EventHandler(this.btnGetMAC_Click);
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Location = new System.Drawing.Point(479, 88);
+            this.btnSendFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(160, 28);
+            this.btnSendFile.TabIndex = 3;
+            this.btnSendFile.Text = "&Send File";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(479, 124);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(160, 28);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Pair
+            // 
+            this.Pair.Location = new System.Drawing.Point(479, 170);
+            this.Pair.Name = "Pair";
+            this.Pair.Size = new System.Drawing.Size(160, 31);
+            this.Pair.TabIndex = 5;
+            this.Pair.Text = "Pair";
+            this.Pair.UseVisualStyleBackColor = true;
+            this.Pair.Visible = false;
+            this.Pair.Click += new System.EventHandler(this.Pair_Click);
+            // 
+            // frmBluetooth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 682);
+            this.Controls.Add(this.Pair);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.btnGetMAC);
+            this.Controls.Add(this.btnDiscover);
+            this.Controls.Add(this.lstDevices);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmBluetooth";
+            this.Text = "Bluetooth";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDevices;
+        private System.Windows.Forms.Button btnDiscover;
+        private System.Windows.Forms.Button btnGetMAC;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button Pair;
+    }
+}
+
---
+++ Bluetooth_PoC_Vishal/Bluetooth_PoC_Vishal/BluetoothPoC/frmBluetooth.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using InTheHand.Net;
+using System.Threading;
+using InTheHand.Net.Sockets;
+using System.IO;
+using InTheHand.Net.Bluetooth;
+
+namespace BluetoothPoC
+{
+    public partial class frmBluetooth : Form
+    {
+        InTheHand.Net.BluetoothAddress[] address_array = new BluetoothAddress[1000];
+        private Thread thrSend;
+        int Selected;
+
+        public frmBluetooth()
+        {
+            InitializeComponent();
+        }
+        private void btnDiscover_Click(object sender, EventArgs e)
+        {
+            BluetoothClient thisDevice = new BluetoothClient();
+            BluetoothDeviceInfo[] devices = thisDevice.DiscoverDevices();
+            for (int i = 0; i < devices.Length; i++)
+            {
+                this.address_array[i] = devices[i].DeviceAddress;
+                this.lstDevices.Items.Add(devices[i].DeviceName);
+            }
+            address_array.Where(x => x != null);
+            Pair.Visible = true;
+        }
+
+        private void btnGetMAC_Click(object sender, EventArgs e)
+        {
+            if (this.lstDevices.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a device.");
+                return;
+            }
+            int index = this.lstDevices.SelectedIndex;
+            string mac = this.address_array[index].ToString();
+            string nap = this.address_array[index].Nap.ToString();
+            string sap = this.address_array[index].Sap.ToString();
+            MessageBox.Show(mac + " " + nap + " " + sap);
+        }
+
+        private void btnSendFile_Click(object sender, EventArgs e)
+        {
+
+            if (this.lstDevices.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a device.");
+                return;
+            }
+            Selected = this.lstDevices.SelectedIndex;
+            sendfile();
+            //this.thrSend = new Thread(new ThreadStart(sendfile));
+            //this.thrSend.Start();
+        }
+
+
+        private string[] FileDialogue()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog1.FileNames;
+            }
+            return null;
+        }
+
+        private void sendfile()
+        {
+            var filepath = FileDialogue();
+            foreach (var x in filepath)
+            {
+                string filename = Path.GetFileName(x);
+                int index = Selected;
+                InTheHand.Net.BluetoothAddress address = this.address_array[index];
+                System.Uri uri = new Uri("obex://" + address.ToString() + "/" + filename);
+                ObexWebRequest request = new ObexWebRequest(uri);
+                request.ReadFile(x);
+                ObexWebResponse response = (ObexWebResponse)request.GetResponse();
+                response.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InTheHand.Net.Sockets.BluetoothClient bc = new InTheHand.Net.Sockets.BluetoothClient();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Pair_Click(object sender, EventArgs e)
+        {
+            PairDevice();
+        }
+        public void PairDevice()
+        {
+            BluetoothClient client = new BluetoothClient();
+            BluetoothDeviceInfo[] devices = client.DiscoverDevices();
+
+            InTheHand.Net.BluetoothAddress address = this.address_array[this.lstDevices.SelectedIndex];
+            foreach (BluetoothDeviceInfo Device in devices)
+            {
+                if (Device.DeviceAddress.Equals(address)) // item from ComboBox
+                {
+                    bool paired = BluetoothSecurity.PairRequest(Device.DeviceAddress, "123456");
+                    if (paired) MessageBox.Show("Paired!");
+                    else MessageBox.Show("There was a problem pairing.");
+                }
+            }
+        }
+    }
+}
